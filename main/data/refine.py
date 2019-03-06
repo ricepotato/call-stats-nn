0c10fb577d8f564e0f52d9af2246121282033c31
Sukjun Sagong <ricepotato40@gmail.com>
2019-03-06 20:33:26 +0900

    api index 기능,test 구현 중

--- main/data/refine.py
+++ main/data/refine.py
@@ -9,27 +9,74 @@ log.addHandler(logging.StreamHandler())
 
 data_path = os.path.dirname(__file__)
 
+API_INDEX_FILENAME = 'apiname_index.csv'
+
 target_obj = {
     'doc_downloader':'doc_downloader_stats.json',
     'gandcrab':'gandcrab_stats.json',
     'java_adwind':'java_adwind_stats.json',
-    'js_downlaoder':'js_downloader_stats.json'
-    'js_gandcrab':'js_gandcrab_stats.json'
+    'js_downlaoder':'js_downloader_stats.json',
+    'js_gandcrab':'js_gandcrab_stats.json',
     'ransom_gen_stats':'ransom_gen_stats.json'
 }
+
+
+
+def api_index(csv_filepath):
+    
+    if not os.path.exists(csv_filepath):
+        log.error('api name index csv file not exist. %s', csv_filepath)
+        return None
+
+    try:
+        with open(csv_filepath, 'r') as f:
+            csv_data = f.read()
+    except Exception as e:
+        log.error('file open error %s', e)
+        return None
+
+    api_index_list = csv_data.split(',')
+    log.debug('api_index_list length=%d', len(api_index_list))
+    api_index_dict = {}
+    for idx, apiname in enumerate(api_index_list):
+        api_index_dict[idx] = apiname
+
+    return api_index_dict
+        
 
 def refine(virus_name, obj):
     apistats = obj[virus_name]
     
+    res_dict = {}
     for task_id, process in apistats.items():
         stats_res = {}
         for pid, calls in process.items():
             for apiname, count in calls.items():
                 stats_res[apiname] = stats_res.get(apiname, 0) + count
+        res_dict[task_id] = stats_res
+
+    return res_dict
+
 
 
 def main():
     log.info('refine data')
+    csv_file = os.path.join(data_path, API_INDEX_FILENAME)
+    api_index = api_index(csv_file)
+    if not api_index:
+        log.error('api_index error')
+        return 0
+
+    for virusname, json_filename in target_obj:
+        json_path = os.path.join(data_path, 'json_data', json_filename)
+        if not os.path.exists(json_path):
+            log.error('path not exists %s', json_path)
+            continue
+
+        with open(json_path, 'r') as f:
+            json_obj = f.read(json_path)
+            res_dict = refine(virusname, json_obj)
+
 
 if __name__ == "__main__":
     main()
--- test/test_refine.py
+++ test/test_refine.py
@@ -4,18 +4,24 @@ import os
 import sys
 import unittest
 import logging
+import json
 
 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 
                                         '..', 'main', 'data'))
+tmp_path = os.path.join(os.path.dirname(__file__), 'tmp')
 
 sys.path.insert(0, base_path)
 
-from refine import refine
-
+from refine import refine, api_index
 
 log = logging.getLogger('csn.test')
 log.setLevel(logging.DEBUG)
 log.addHandler(logging.StreamHandler())
+
+try:
+    os.mkdir(tmp_path)
+except Exception as e:
+    log.warning('mkdir tmp path=%s, %s',tmp_path, e)
 
 test_obj = {
     "java_adwind": {
@@ -28,36 +34,20 @@ test_obj = {
                 "NtQueryValueKey": 5
             }, 
             "3683": {
-                "LdrUnloadDll": 1, 
-                "LdrGetDllHandle": 2, 
-                "NtOpenFile": 1, 
                 "FindFirstFileExW": 3, 
                 "SetFileInformationByHandle": 1, 
-                "NtQueryValueKey": 1
+                "DeviceIoControl": 1, 
+                "NtQueryInformationFile":3
             }            
             },
         "2867200": {
             "3560": {
                 "LdrUnloadDll": 1, 
                 "DeviceIoControl": 20, 
-                "NtSetInformationFile": 519, 
-                "GetSystemWindowsDirectoryW": 1, 
-                "NtClose": 1646, 
-                "NtCreateSection": 1, 
-                "NtOpenKey": 3, 
-                "LdrGetProcedureAddress": 2, 
-                "LdrLoadDll": 1, 
-                "NtTerminateProcess": 3, 
-                "NtQueryInformationFile": 1880, 
-                "NtQueryValueKey": 5
             }, 
             "3688": {
                 "LdrUnloadDll": 1, 
                 "LdrGetDllHandle": 2, 
-                "NtOpenFile": 1, 
-                "FindFirstFileExW": 3, 
-                "SetFileInformationByHandle": 1, 
-                "NtQueryValueKey": 1
             }
         }
     }
@@ -65,13 +55,38 @@ test_obj = {
 
 class TestRefine(unittest.TestCase):
     def setUp(self):
-        self.model = Model()
+        pass
 
     def tearDown(self):
         pass
 
     def test_refine(self):
-        res_obj = refine(test_obj, 'java_adwind')
+        res_obj = refine('java_adwind', test_obj)
+        # api call 수가 모든 pid 의 call 수를 모두 합친것과 같아야 한다.
+        self.assertEqual(res_obj['2867201']['LdrUnloadDll'], 1)
+        self.assertEqual(res_obj['2867201']['DeviceIoControl'], 21)
+        self.assertEqual(res_obj['2867201']['NtQueryInformationFile'], 1883)
+        self.assertEqual(res_obj['2867201']['NtQueryValueKey'], 5)
+
+        self.assertEqual(res_obj['2867200']['LdrUnloadDll'], 2)
+        self.assertEqual(res_obj['2867200']['LdrGetDllHandle'], 2)
+        self.assertEqual(res_obj['2867200']['DeviceIoControl'], 20)
+
+        #log.info(res_obj)
+
+    def test_api_index(self):
+        test_csv_path = os.path.join(os.path.dirname(__file__), 'test_csv.csv')
+        with open(test_csv_path, 'w') as f:
+            f.write('apiname1,apiname2,apiname3')
+
+        res = api_index(test_csv_path)
+
+        # csv 파싱 하여 결과가 3이어야 한다.
+        self.assertEqual(len(res), 3)
+        # index 에 맞는 문자열이 출력되어야 한다.
+        self.assertEqual(res[0], 'apiname1')
+        self.assertEqual(res[1], 'apiname2')
+        self.assertEqual(res[2], 'apiname3')
 
 if __name__ == "__main__":
     unittest.main()
